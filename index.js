593fd8fd82d47a5024b47ef61f7afad889bf5a88
yuekcc <yuekcc@qq.com>
2023-10-27 00:38:57 +0800

    更新样式

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { llama } from '/completion.js';
+import { llama } from './completion';
 
 const state = {
   id: 0,
@@ -8,8 +8,8 @@ const state = {
 };
 
 function formatMessage(prompt, response = '', systemPrompt = '') {
-  let systemPrompt_ = systemPrompt ? `<<SYS>>${systemPrompt}<</SYS>>` : '';
-  return ` [INST]${systemPrompt_} ${prompt} [/INST]${response}`;
+  let systemPrompt_ = systemPrompt ? `\n<<SYS>>${systemPrompt}<</SYS>>\n` : '';
+  return `[INST]${systemPrompt_} ${prompt} [/INST]\n${response}`;
 }
 
 function formatHistory(history) {
@@ -17,14 +17,11 @@ function formatHistory(history) {
   return messages.map(m => formatMessage(m.prompt, m.response)).join('\n');
 }
 
-export async function send(prompt, { systemPrompt = '', config = {}, writer }) {
+async function send(prompt, { systemPrompt = '', config = {}, writer }) {
   state.systemPrompt = systemPrompt;
   state.config = config || {};
 
-  console.log(state);
   const finalPrompt = `${formatHistory(state.history)}${formatMessage(prompt, '', state.id === 0 ? systemPrompt : '')}`;
-  console.log(finalPrompt);
-  console.log(state);
 
   const id = state.id++;
   state.history[id] = {
@@ -43,3 +40,63 @@ export async function send(prompt, { systemPrompt = '', config = {}, writer }) {
 
   state.history[id].response = response;
 }
+
+const $ = document.querySelector.bind(document);
+
+const systemPrompt = $('#system_prompt');
+const prompt = $('#prompt');
+const llmSettings = $('#llm_settings');
+
+function parseJson(doc, defaultValue = null) {
+  try {
+    return JSON.parse(doc);
+  } catch (e) {
+    console.warn('#parseJson', e);
+    return defaultValue;
+  }
+}
+
+function div(className, ...children) {
+  const element = document.createElement('div');
+  element.className = className;
+  element.append(...children);
+  return element;
+}
+
+const messageContainer = document.querySelector('.message-container');
+
+function makeMessageContainers() {
+  const responseContent = div('content');
+  const promptContent = div('content');
+  const appendedContent = div('content');
+
+  const message = div('message', div('prompt', promptContent), div('response', responseContent), div('appended', appendedContent));
+  messageContainer.append(message);
+
+  return { responseContent, promptContent, appendedContent };
+}
+
+function onSend() {
+  const systemPrompt_ = systemPrompt.value.trim();
+  const prompt_ = prompt.value.trim();
+  const llmSettings_ = parseJson(llmSettings.value.trim());
+
+  prompt.value = '';
+
+  const { responseContent, promptContent, appendedContent } = makeMessageContainers();
+
+  const writer = text => {
+    responseContent.textContent += text;
+    appendedContent.scrollIntoView({ behavior: 'smooth', block: 'end' });
+  };
+
+  promptContent.textContent = prompt_;
+  send(prompt_, { systemPrompt: systemPrompt_, writer, config: llmSettings_ });
+}
+
+$('#send').addEventListener('click', onSend);
+$('#prompt').addEventListener('keydown', evt => {
+  if (evt.ctrlKey && evt.keyCode === 13) {
+    onSend();
+  }
+});
